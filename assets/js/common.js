50ad0274a96bed3cf8ceb90b4b9b1cf16e613b77
Myo Htet <myohetspringgithub@gmail.com>
2022-04-20 22:54:30 +0630

    trigger

--- assets/js/common.js
+++ assets/js/common.js
@@ -103,13 +103,11 @@ jQuery(function($) {
         }
       });
     }
-
-    $(window).scroll(function () {
-      fadeAnime(); //call function fadeAnime
-    });
+    function fadeAnime(){
     $(".fadeUpTrigger").each(function () {
       //fadeUpTriggerというクラス名が
       var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
       if (scroll >= elemPos - windowHeight) {
@@ -117,6 +115,11 @@ jQuery(function($) {
       } else {
         $(this).removeClass("fadeUp"); // 画面外に出たらfadeUpというクラス名を外す
       }
+    });
+  }
+
+    $(window).scroll(function () {
+      fadeAnime(); //call function fadeAnime
     });
     $(function() {
       $('.new-list li').matchHeight();
